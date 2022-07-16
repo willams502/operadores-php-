4ff5be2660bf3e5aaac30e7a041a9c13b1664a0b
Willams <84189241+willams502@users.noreply.github.com>
2022-07-16 12:36:32 -0300

    Material de leitura
    https://www.w3schools.com/php/php_operators.asp

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+        /*operadores 
+        -> São aqueles sinais que usamos para atribuir valores, comparar valores, fazer calculos e etc...
+        Exc: + - * / = ++ -- += && 
+
+        1) Operadores aritimeticos (matématicos);
+        1) Operadores de Atribuição;
+        1) Operadires de Sequencia (Concatenação);
+        4)Operadores de ccomparação;
+        5) Operador condicional (Ternário);
+        6)Operadores Lógicos;
+
+        São os mesmo do java script;
+        */
+
+
+        //Java scrip
+       // var nome = "Dimirtri";
+        //var sobrenome = "Teixeira";
+        //var nome-completo= nome + sobrenome;
+
+
+        //php  no php se usa o .
+
+        $nome = "Matheus";
+        $sobrenome = "Willams";
+        $nome_completo = $nome . $sobrenome;
+
+?>
